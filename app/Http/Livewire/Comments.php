114376f9c420d3f0dbfe2ecb9cfd06b2b81e9d52
Alperen Öztaş <alprnoztas@gmail.com>
2022-07-09 16:13:49 +0300

    real time validation with livewire

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -16,20 +16,23 @@ class Comments extends Component
 
 
     public function mount(){
-        $initialComments = Comment::all();
+        $initialComments = Comment::latest()->get();
         $this->comments = $initialComments;
+    }
+
+    public function updated($field){
+        $this->validateOnly($field,[
+            'newComment'=>'required|max:255'
+        ]);
     }
 
     public function addComment(){
 
-        if($this->newComment == ''){
-            return;
-        }
-        array_unshift($this->comments,[
-            'body' => $this->newComment,
-            'created_at' => Carbon::now()->diffForHumans(),
-            'creator' => 'Ayla'
-        ]);
+        $this->validate(['newComment'=>'required']);
+
+        $createdComment = Comment::create(['body'=>$this->newComment,'user_id'=> 1]);
+
+        $this->comments->prepend($createdComment);
 
         $this->newComment="";
     }
