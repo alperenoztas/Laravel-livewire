c1b6bd3acce3e6841d8f746d07a12c7ae2ed5cd2
Alperen Öztaş <alprnoztas@gmail.com>
2022-07-11 10:54:49 +0300

    Delete operation and flash message

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -43,6 +43,17 @@ class Comments extends Component
         $this->comments->prepend($createdComment);
 
         $this->newComment="";
+
+        session()->flash('message','Comment added successfully');
+    }
+
+    public function remove($commentId){
+        $comment = Comment::find($commentId)->delete();
+
+        $this->comments = $this->comments->where('id','!=',$commentId);
+
+        session()->flash('message','Comment deleted successfully');
+
     }
 
     public function render()
